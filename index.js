b74abe79541b130eeeefb9a1976b8bce98718908
Evan Cooke <evncooke@gmail.com>
2022-02-21 19:47:08 -0600

    Added comments

---
+++ index.js
@@ -0,0 +1,5 @@
+// object whichs gives access to all other objects and classes to make audio
+const audioContext = new AudioContext();
+
+// aduio buffer with sample rate passed in
+console.log(audioContext.sampleRate)
