9a710134d7763f15902bc5471eab7fc1ed650ca1
Evan Cooke <evncooke@gmail.com>
2022-02-21 20:04:16 -0600

    Added buffer parameters

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 const audioContext = new AudioContext();
 
 // aduio buffer with sample rate passed in
-console.log(audioContext.sampleRate)
+// print sample rate: console.log(audioContext.sampleRate)
+const buffer = audioContext.createBuffer()
+	1. // 1 channel (mono auido)
+	audioContext.sampleRate * 1 // # of samples in entire buffer = sample rate * seconds in sample
+	audioContext.sampleRate // pass in sample rate of buffer
